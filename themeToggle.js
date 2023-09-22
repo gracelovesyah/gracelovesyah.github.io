25b9532d486d05e503231cb1adb89cb190e4d430
gracelovesyah <jiahe3@student.unimelb.edu.au>
2023-09-22 11:59:57 +1000

    resume

--- themeToggle.js
+++ themeToggle.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    // Initialization: Check the current theme in localStorage
     const currentTheme = localStorage.getItem("theme");
-    if (currentTheme == "dark") {
-        document.body.setAttribute("data-theme", "dark");
+    if (currentTheme === "dark") {
+        document.body.classList.add("dark");
     }
 
+    // Toggle: Add an event listener to the theme toggle button
     document.querySelector("#theme-toggle").addEventListener("click", function() {
-        if (document.body.getAttribute("data-theme") == "dark") {
-            document.body.removeAttribute("data-theme");
+        if (document.body.classList.contains("dark")) {
+            document.body.classList.remove("dark");
             localStorage.removeItem("theme");
         } else {
-            document.body.setAttribute("data-theme", "dark");
+            document.body.classList.add("dark");
             localStorage.setItem("theme", "dark");
         }
     });
